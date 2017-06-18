1ae9208ed61804afa349be979d32554bef71b291
mark <h091237557@gmail.com>
2017-06-18 16:16:15 +0800

    fix: fix route bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,82 +1,91 @@
 import {
-  NgModule
+    NgModule,
+    provide
 } from '@angular/core';
 import {
-  BrowserModule
+    BrowserModule
 } from '@angular/platform-browser';
 import {
-  RouterModule,
-  Routes
+    RouterModule,
+    Routes
 } from '@angular/router';
+
 import {
-  AppComponent
+    HashLocationStrategy,
+    Location,
+    LocationStrategy
+} from '@angular/common';
+
+import {
+    AppComponent
 } from './app.component';
 import {
-  CreateApiComponent
+    CreateApiComponent
 } from './Components/create-api/create-api.component.ts';
 import {
-  ViewApisComponent
+    ViewApisComponent
 } from './Components/view-apis/view-apis.component.ts';
 
 import {
-  ViewDetailsApiComponent
+    ViewDetailsApiComponent
 } from './Components/viewDetails-api/viewDetails-api.component.ts';
 
 import {
-  ViewModalApiComponent
+    ViewModalApiComponent
 } from './Components/viewModal-api/viewModal-api.component.ts';
 
 import {
-	TestGetApiComponent
+    TestGetApiComponent
 } from './Components/testGet-api/testGet-api.component';
 
 import {
-  FormsModule
+    FormsModule
 } from '@angular/forms';
 import {
-  HttpModule
+    HttpModule
 } from '@angular/http';
 
 import {
-  JsonDataService
+    JsonDataService
 } from './Services/jsonData.service';
 
 
 const routes: Routes = [{
-  path: '',
-  redirectTo: '/view',
-  pathMatch: 'full'
+    path: '',
+    redirectTo: '/view',
+    pathMatch: 'full'
 }, {
-  path: 'view',
-  component: ViewApisComponent
+    path: 'view',
+    component: ViewApisComponent
 }, {
-  path: 'create',
-  component: CreateApiComponent
-},{
-	path: 'view/:id',
-	component: ViewDetailsApiComponent,
-	outlet : 'modal'
+    path: 'create',
+    component: CreateApiComponent
+}, {
+    path: 'view/:id',
+    component: ViewDetailsApiComponent,
+    outlet: 'modal'
 }];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ],
-  declarations: [
-    AppComponent,
-    CreateApiComponent,
-    ViewApisComponent,
-    ViewDetailsApiComponent,
-    ViewModalApiComponent,
-		TestGetApiComponent
-  ],
-  providers: [JsonDataService],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(routes, { useHash: true })
+    ],
+    exports: [
+        RouterModule
+    ],
+    declarations: [
+        AppComponent,
+        CreateApiComponent,
+        ViewApisComponent,
+        ViewDetailsApiComponent,
+        ViewModalApiComponent,
+        TestGetApiComponent
+    ],
+    providers: [JsonDataService],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule {}
